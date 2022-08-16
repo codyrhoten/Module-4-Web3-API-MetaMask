7c3e7c814124b1d9b581d7a34898a55a40a3aaf9
codyrhoten <74472753+codyrhoten@users.noreply.github.com>
2022-08-15 20:01:11 -0400

    complete 2_deploy_contracts.js

--- exercise-decentralized-casino/migrations/2_deploy_contracts.js
+++ exercise-decentralized-casino/migrations/2_deploy_contracts.js
@@ -3,5 +3,14 @@
 const Casino = artifacts.require("Casino");
 
 module.exports = function(deployer) {
-  // TODO: Implementation
+    const minimumBet = "0.1";
+    const maxNumberOfBets = "2";
+    const minimumBetEthers = web3.utils.toWei(minimumBet, "ether");
+
+    deployer.deploy(
+        Casino, 
+        minimumBetEthers, 
+        maxNumberOfBets, 
+        { gas: 5000000 }
+    );
 };
